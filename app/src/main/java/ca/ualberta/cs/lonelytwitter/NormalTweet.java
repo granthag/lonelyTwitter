c5729aaab7871ef1dcd5bbfffe1d6ce2eb30b021
granthag <granthag@ualberta.ca>
2018-01-18 18:16:45 -0700

    Lab 2 changes

---
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/NormalTweet.java
@@ -0,0 +1,24 @@
+package ca.ualberta.cs.lonelytwitter;
+
+import java.util.Date;
+
+/**
+ * Created by micag on 2018-01-18.
+ */
+
+public class NormalTweet extends Tweet {
+
+    public NormalTweet (String message) {
+        super(message);
+    }
+
+    public NormalTweet(String message, Date date) {
+        super(message,date);
+    }
+
+    @Override
+    public Boolean isImportant() {
+        return Boolean.FALSE;
+    }
+
+}
