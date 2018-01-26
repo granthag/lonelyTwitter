2af4554a4abab8007fea29c4e1384fa174e1a8d2
granthag <granthag@ualberta.ca>
2018-01-25 19:02:49 -0700

    Clear button working

--- app/src/main/java/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivity.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivity.java
@@ -73,7 +73,8 @@ public class LonelyTwitterActivity extends Activity {
 			public void onClick(View v) {
 				setResult(RESULT_OK);
 				String text = bodyText.getText().toString();
-
+				tweetlist.clear();
+				adapter.notifyDataSetChanged();
 
 	}});}
 
