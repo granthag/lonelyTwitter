2e779494800dc0c7e566cf80951d3e10825aba47
granthag <granthag@ualberta.ca>
2018-01-25 18:51:33 -0700

    Lab presentation

--- app/src/main/java/ca/ualberta/cs/lonelytwitter/Tweet.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/Tweet.java
@@ -44,4 +44,10 @@ public abstract class Tweet implements Tweetable {
     }
 
     public abstract Boolean isImportant();
+
+    @Override
+    public String toString() {
+        return date.toString() + " | " + message;
+        //return getMessage();
+    }
 }
